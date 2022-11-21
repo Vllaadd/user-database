b38287c2edc74ae7285326bc9c985521fa2a2a7c
Vllaadd <vlladdiimiir@gmail.com>
2022-11-21 03:14:18 -0500

    programming languages app

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+  <>
+  <div>
+    <div>
+      <h1>Hello, World</h1>
+      <p>THis is a simple home page</p>
+    <Link to="/saerch?q=Java">Java</Link>
+    <Link to="/saerch?q=Python">Python</Link>
+    <Link to="/saerch?q=JavaScript">JavaScript</Link>
+    </div>
+  </div>
+  </>
   )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Search() {
+    const [language, setLanguage] = useState("");
+    const [input, setInput] = useState("");
+    return (
+        <>
+            <div>
+                <div>
+                    <Link to="/" >Back to home</Link>
+                    <h1>{language}</h1>
+                    <p><span>{language}</span>is a very cool programming language.
+                        It is used by many develipers from all around the world. Every
+                        programmer should have heard of <span>{language}</span> before.</p>
+                    <form onSubmit={submitAction}></form>
+                    <input type="text"
+                        placeholder='Type language'
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}></input>
+                    <div>
+                        <button type='submit'>Go!</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
