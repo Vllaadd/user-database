177142af1971efd3fd33b41ba1191b9a20b9713d
Vllaadd <vlladdiimiir@gmail.com>
2022-11-27 05:14:02 -0500

    style added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import  './ExpenseItem.css';
+
 
 export default function ExpenseItem() {
-  return (
-    <div><p>Expense Item Page.</p>git </div>
-  )
+    return (
+        <>
+           <div className='expense-item'>
+            <div>November 27 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$294.67</div>
+            </div>
+           </div>
+        </>
+
+    )
 }
