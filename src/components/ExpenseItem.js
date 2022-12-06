dd90587008f35e449f7d9bf0e0d5ff92d390cd02
Vllaadd <vlladdiimiir@gmail.com>
2022-12-06 03:21:29 -0500

    minor changes

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import  './ExpenseItem.css';
 
+export default function ExpenseItem(props) {
 
-export default function ExpenseItem() {
     return (
         <>
-           <div className='expense-item'>
-            <div>November 27 2022</div>
-            <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
+            <div className='expense-item'>
+                <div>{props.date.toISOString()}</div>
+                <div className='expense-item__description'>
+                    <h2>{props.title}</h2>
+                    <div className='expense-item__price'>{props.amount}</div>
+                </div>
             </div>
-           </div>
         </>
 
     )
