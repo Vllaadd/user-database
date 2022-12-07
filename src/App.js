4fb4494b5a4fbaf9a57308601fe3bd3223f94bc3
Vllaadd <vlladdiimiir@gmail.com>
2022-12-07 03:03:08 -0500

    minor errors fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React from 'react';
 import Expenses from './components/Expenses';
 
 
-export default function App() {
-	
+export default function App(props) {
+	const expenses = [
+		{
+			id: 'ci',
+			title: 'Car Insurance',
+			amount: 294.67,
+			date: new Date(2021, 2, 28)
+		},{
+			id: 'gr',
+			title: 'Groceries',
+			amount: 73,
+			date: new Date(2021, 2, 28)
+		},{
+			id: 'cr',
+			title: 'Car rental',
+			amount: 73,
+			date: new Date(2021, 2, 28)
+		}
+	]
 	return (
 		<>
 			<h3>Let's get started!</h3>
-			<Expenses />
+			<Expenses items={expenses} />
 		</>
 	)
 };
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,42 +1,25 @@
 import ExpenseItem from './ExpenseItem';
 
 function Expenses(props){
-    const expenses = [
-		{
-			id: 'ci',
-			title: 'Car Insurance',
-			amount: 294.67,
-			date: new Date(2021, 2, 28)
-		},{
-			id: 'gr',
-			title: 'Groceries',
-			amount: 73,
-			date: new Date(2021, 2, 28)
-		},{
-			id: 'cr',
-			title: 'Car rental',
-			amount: 73,
-			date: new Date(2021, 2, 28)
-		}
-	]
-
     return(
-        <div>
+        <div className='expenses'>
             <ExpenseItem 
-				title={props.expenses[0].title}
-				amount={props.expenses[0].amount}
-				date={props.expenses[0].date} 
+				title={props.items[0].title}
+				amount={props.items[0].amount}
+				date={props.items[0].date} 
 				/>
 				<ExpenseItem 
-				title={props.expenses[1].title}
-				amount={props.expenses[1].amount}
-				date={props.expenses[1].date} 
+				title={props.items[1].title}
+				amount={props.items[1].amount}
+				date={props.items[1].date} 
 				/>
 				<ExpenseItem 
-				title={props.expenses[2].title}
-				amount={props.expenses[2].amount}
-				date={props.expenses[2].date} 
+				title={props.items[2].title}
+				amount={props.items[2].amount}
+				date={props.items[2].date} 
 				/>
         </div>
     )
 }
+
+export default Expenses;
