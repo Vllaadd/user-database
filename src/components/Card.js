71a7eedaadc328cdf0c841cb89ef5b96f03d8b1f
Vllaadd <vlladdiimiir@gmail.com>
2022-12-07 05:09:46 -0500

    minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import './styling/Card.css';
 
 function Card(props){
+    const classes = 'card' + props.className;
     return(
-    <div className="card">{props.children}</div>
+    <div className={classes}>{props.children}</div>
     )
     
 }
