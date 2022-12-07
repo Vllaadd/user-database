808caf637610d7770fa7be477290f080a0b8cf34
Vllaadd <vlladdiimiir@gmail.com>
2022-12-07 01:57:47 -0500

    expenses file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 
 export default function App() {
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,5 @@
+import ExpenseItem from './ExpenseItem';
+
 function Expenses(props){
     const expenses = [
 		{
