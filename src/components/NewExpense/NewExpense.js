de5a3dcb4812385f21f55f2739af5e87c4c61850
Vllaadd <vlladdiimiir@gmail.com>
2022-12-09 05:38:11 -0500

    data passed from child to parents

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,11 +2,18 @@ import ExpenseForm from './ExpenseForm';
 import '../styling/NewExpense.css';
 
 const NewExpense = () => {
+    saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        console.log(expenseData);
+    };
     return(
-        <div>
-        <ExpenseForm />
+        <div className='new-expense'>
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
-    ) 
+    )  
 };
 
 export default NewExpense;
